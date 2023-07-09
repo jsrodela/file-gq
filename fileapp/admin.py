fcce8a72dfffa8d97be2b963cd47a218c8b63788
HURDOO <64835865+HURDOO@users.noreply.github.com>
2023-07-09 15:17:48 +0000

    File.gq Launching 0710

--- fileapp/admin.py
+++ fileapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class UploadModel(admin.ModelAdmin):
+  list_display = ['id', 'code', 'temporary', 'time', 'index', ]
+  list_display_links = ['id', 'code']
+
+class FileUploadModel(admin.ModelAdmin):
+  list_display = ['id', 'upload_id']
+  list_display_links = ['id']
+  
+admin.site.register(models.Upload, UploadModel)
+admin.site.register(models.FileUpload, FileUploadModel)
--- fileapp/views.py
+++ fileapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import random
 
 from . import models
@@ -6,25 +6,48 @@ from . import models
 
 # Create your views here.
 def index(request):
-    return render(request, 'fileapp/index.html', {
-        'hello': 'world',
-    })
+    return render(request, 'fileapp/index.html', {})
 
 
 def send(request):
     if request.method == "POST":
         code = genCode()
-        return render(request, 'fileapp/send_result.html', {})
+        file = request.FILES["file"]
+
+        upload = models.Upload(code=code, )
+        upload.save()  # Upload 모델
+
+        fileupload = models.FileUpload(upload_id=upload, file=file)
+        fileupload.save()
+        return render(request, 'fileapp/send_result.html', {'code': code})
 
     return render(request, 'fileapp/send.html', {})
 
 
 def receive(request):
+    if request.method == 'POST':
+        code = request.POST['code']
+    else:
+        code = request.GET.get('code', None)
+
+    if code is not None:
+        if code == '':
+            return render(request, 'fileapp/receive.html', {'error': ''})
+
+        upload = models.Upload.objects.filter(code=int(code)).first()
+        if upload is None:
+            return render(request, 'fileapp/receive.html', {'error': ''})
+
+        file = models.FileUpload.objects.filter(upload_id=upload).first()
+        if file is None:
+            return render(request, 'fileapp/receive.html', {'error': ''})
+
+        return redirect(file.file.url)
+
     return render(request, 'fileapp/receive.html', {})
 
 
 def send_result(request):
-
     return render(request, 'fileapp/send_result.html', {})
 
 
