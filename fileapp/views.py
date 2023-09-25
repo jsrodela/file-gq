030576c6b3f0b81c8a771c6c3403fcf44a88be12
HURDOO <64835865+HURDOO@users.noreply.github.com>
2023-09-25 02:03:54 +0000

    Serve file as response

--- fileapp/views.py
+++ fileapp/views.py
@@ -3,6 +3,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 import random
 import json
+from django.core.files.storage import FileSystemStorage
+from django.http import FileResponse
+from urllib import parse
+
 from . import models
 
 
@@ -29,24 +33,42 @@ def send(request):
 def receive(request):
     if request.method == 'POST':
         code = request.POST['code']
+
+        if code is not None:
+            if code == '':
+                return render(request, 'fileapp/receive.html', {'error': ''})
+    
+            upload = models.Upload.objects.filter(code=int(code)).first()
+            if upload is None:
+                return render(request, 'fileapp/receive.html', {'error': ''})
+
+            # file = models.FileUpload.objects.filter(upload_id=upload).first()
+            # if file is None:
+            #     return render(request, 'fileapp/receive.html', {'error': ''})
+    
+            response = get_file_response(upload)
+            if response is None:
+                return render(request, 'fileapp/receive.html', {'error': ''})
+
+            return response
+            
+            # return redirect(file.file.url)
     else:
-        code = request.GET.get('code', None)
+        # code = request.GET.get('code', None)
+        return render(request, 'fileapp/receive.html', {})
 
-    if code is not None:
-        if code == '':
-            return render(request, 'fileapp/receive.html', {'error': ''})
 
-        upload = models.Upload.objects.filter(code=int(code)).first()
-        if upload is None:
-            return render(request, 'fileapp/receive.html', {'error': ''})
+def get_file_response(upload):
+  file_upload = models.FileUpload.objects.filter(upload_id=upload).first()
+  if file_upload is None:
+      return None
+  file_path = file_upload.file.path
+  file_name = file_upload.file.name
+  fs = FileSystemStorage(file_path)
 
-        file = models.FileUpload.objects.filter(upload_id=upload).first()
-        if file is None:
-            return render(request, 'fileapp/receive.html', {'error': ''})
-
-        return redirect(file.file.url)
-
-    return render(request, 'fileapp/receive.html', {})
+  response = FileResponse(fs.open(file_path, 'rb'), content_type='multipart/form-data;')
+  response['Content-Disposition'] = 'attachment; filename*=UTF-8\'\'%s' % parse.quote(file_name)
+  return response
 
 
 def send_result(request):
